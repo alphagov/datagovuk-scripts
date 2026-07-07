6cec8be6821854c913110ca51f8a6bce0594c700
Brendan Smith <brendan.smith@digital.cabinet-office.gov.uk>
2026-07-07 16:27:21 +0100

    Fix lint complaints

--- check-links-analysis/scripts/filter.py
+++ check-links-analysis/scripts/filter.py
@@ -1,6 +1,6 @@
 import argparse
 import csv
-from collections import defaultdict
+
 
 def is_row_excluded(row, exclusions, exclusion_substrings):
     for exclusion_column, exclusion_values in exclusions.items():
@@ -27,18 +27,36 @@ def filter(input_file_path, output_file_path, exclusions, exclusion_substrings):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="A script to filter broken link CSVs for data.gov.uk")
+    parser = argparse.ArgumentParser(
+        description="A script to filter broken link CSVs for data.gov.uk"
+    )
     parser.add_argument("input_file_path", type=str, help="The CSV file to filter")
-    parser.add_argument("output_file_path", type=str, help="The file path to write the filtered CSV to")
-    parser.add_argument("-e", "--exclude", action="append", type=str, help="The column(s)/values to exclude")
-    parser.add_argument("-ei", "--exclude-substring", action="append", type=str, help="The column(s)/values to exclude if exclusion value is a substring of a particular row's value")
+    parser.add_argument(
+        "output_file_path", type=str, help="The file path to write the filtered CSV to"
+    )
+    parser.add_argument(
+        "-e",
+        "--exclude",
+        action="append",
+        type=str,
+        help="The column(s)/values to exclude",
+    )
+    parser.add_argument(
+        "-ei",
+        "--exclude-substring",
+        action="append",
+        type=str,
+        help="The column(s)/values to exclude if exclusion value is a substring of a particular row's value",
+    )
     args = parser.parse_args()
     exclusions = {}
     for exclude_argument in args.exclude:
         try:
             column_name, value = exclude_argument.split("=")
         except ValueError:
-            raise Exception(f"--exclude argument {exclude_argument} did not follow <column>=<value> pattern")
+            raise Exception(
+                f"--exclude argument {exclude_argument} did not follow <column>=<value> pattern"
+            )
         if "," in value:
             value = value.split(",")
         else:
@@ -49,13 +67,17 @@ def main():
         try:
             column_name, value = exclude_argument.split("=")
         except ValueError:
-            raise Exception(f"--exclude-substring argument {exclude_argument} did not follow <column>=<value> pattern")
+            raise Exception(
+                f"--exclude-substring argument {exclude_argument} did not follow <column>=<value> pattern"
+            )
         if "," in value:
             value = value.split(",")
         else:
             value = [value]
         exclusion_substrings[column_name] = value
-    filter(args.input_file_path, args.output_file_path, exclusions, exclusion_substrings)
+    filter(
+        args.input_file_path, args.output_file_path, exclusions, exclusion_substrings
+    )
 
 
 if __name__ == "__main__":
