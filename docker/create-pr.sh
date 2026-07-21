abb39dc0cb917eb7a9228e3716707487170a9607
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2026-07-21 10:47:48 +0100

    wip Update create-pr.sh to handle check-links-ckan
    Update check-links.yaml and check-links-ckan.yaml in the same PR

--- docker/create-pr.sh
+++ docker/create-pr.sh
@@ -18,16 +18,25 @@ git clone https://github.com/alphagov/govuk-dgu-charts.git charts
 
 cd charts/charts/ckan/images
 
+HAS_CHANGED=0
 for ENV in $(echo $ENVS | tr "," " "); do
   (
     cd "${ENV}"
-    yq -i '.tag = env(IMAGE_TAG)' "check-links.yaml"
-    yq -i '.branch = env(SOURCE_BRANCH)' "check-links.yaml"
-    git add "check-links.yaml"
+    for SUFFIX in ["", "-ckan"]; do
+      (
+        yq -i '.tag = env(IMAGE_TAG)' "check-links${SUFFIX}.yaml"
+        yq -i '.branch = env(SOURCE_BRANCH)' "check-links${SUFFIX}.yaml"
+        git add "check-links${SUFFIX}.yaml"
 
-    if git diff --cached --quiet -- "check-links.yaml"; then
-      echo "Nothing to commit"
-    else
+        if git diff --cached --quiet -- "check-links${SUFFIX} .yaml"; then
+          echo "Nothing to commit for check-links${SUFFIX}.yaml in ${ENV}"
+        else
+          HAS_CHANGED=1
+        fi
+      )
+    done
+
+    if [[ $HAS_CHANGED -eq 1 ]]; then
       BRANCH="ci/${IMAGE_TAG}-${ENV}"
       # Check remote (not local) — git show-ref only sees local refs in a fresh clone
       if git ls-remote --exit-code --heads origin "${BRANCH}" >/dev/null 2>&1; then
@@ -37,7 +46,7 @@ for ENV in $(echo $ENVS | tr "," " "); do
         git checkout -b "${BRANCH}" origin/main
         git commit -m "Update check-links image tags for ${ENV} to ${IMAGE_TAG}"
         git push --set-upstream origin "${BRANCH}"
-        gh pr create --title "Update check-links image tags for ${ENV} (${IMAGE_TAG})" --base main --head "${BRANCH}" --fill --repo alphagov/govuk-dgu-charts
+        # gh pr create --title "Update check-links image tags for ${ENV} (${IMAGE_TAG})" --base main --head "${BRANCH}" --fill --repo alphagov/govuk-dgu-charts
       fi
     fi
   )
